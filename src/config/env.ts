d86aed687496a9b199f0ad48b396203ac4863f1d
PELUMIJOY <faslusky@gmail.com>
2023-02-03 22:38:51 +0100

    updated file

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,7 @@ dotenv.config();
 
 export default {
     FILE_HOST:
-    process.env.NODE_ENV === "tevelopment"
+    process.env.NODE_ENV === "development"
          ? "http://localhost:3120"
-         :"http://localhost:3120" 
-        //  process.env.PRODUCTION_URL
+         : process.env.PRODUCTION_URL
 }
